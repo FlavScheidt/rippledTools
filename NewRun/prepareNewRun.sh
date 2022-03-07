e63047b2f851ebc217214f6dfaa4eedba5e4bb08
Flaviene <flaviene.scheidt@uni.lu>
2022-03-07 22:21:11 +0100

    Checking if logs exist

--- NewRun/prepareNewRun.sh
+++ NewRun/prepareNewRun.sh
@@ -37,36 +37,36 @@ NEW_GRPCLOGS_DIR="/root/sntrippled/grpc/logs"
 ##########################
 readarray -t nodes < ./nodes.txt
 
-for node in "${nodes}";
+for n in "${nodes[@]}";
 do
-	echo ${node}
+	echo ${n}
 
 	##########################
 	#	CHECK NEW DIRECTORIES
 	##########################
-	if ssh ${node} "[ ! -d $NEW_LOGS_DIR ]"
+	if ssh ${n} "[ ! -d $NEW_LOGS_DIR ]"
 	then
-		ssh ${node} "mkdir $NEW_LOGS_DIR"
+		ssh ${n} "mkdir $NEW_LOGS_DIR"
 	fi
 
-	if ssh ${node} "[ ! -d $NEW_DB_DIR ]"
+	if ssh ${n} "[ ! -d $NEW_DB_DIR ]"
 	then
-		ssh ${node} "mkdir $NEW_DB_DIR"
+		ssh ${n} "mkdir $NEW_DB_DIR"
 	fi
 
 	#Specifics for snt
-	if ssh ${node} "[ ! -d $NEW_STDOUTLOGS_DIR ]"
+	if ssh ${n} "[ ! -d $NEW_STDOUTLOGS_DIR ]"
 	then
-		ssh ${node} "mkdir $NEW_STDOUTLOGS_DIR"
+		ssh ${n} "mkdir $NEW_STDOUTLOGS_DIR"
 	fi
 
-	if ssh ${node} "[ ! -d $NEW_GRPCLOGS_DIR ]"
+	if ssh ${n} "[ ! -d $NEW_GRPCLOGS_DIR ]"
 	then
-		ssh ${node} "mkdir $NEW_GRPCLOGS_DIR"
+		ssh ${n} "mkdir $NEW_GRPCLOGS_DIR"
 	fi
 
 
-	if ssh ${node} "[ -f ${LOGS_DIR}/debug.log ]"
+	if ssh ${n} "[ -f ${LOGS_DIR}/debug.log ]"
 	then
 
 		#########################
@@ -74,21 +74,21 @@ do
 		#########################
 		#Rename the last log with the timestamp a the begining of the log
 
-		LOG_DATE=$(ssh ${node} "head -1 ${LOGS_DIR}/debug.log | cut -d ' ' -f1")
-		LOG_HOUR=$(ssh ${node} "head -1 ${LOGS_DIR}/debug.log | cut -d ' ' -f2 | cut -d ':' -f1")
-		LOG_MIN=$(ssh ${node} "head -1 ${LOGS_DIR}/debug.log | cut -d ' ' -f2 | cut -d ':' -f2")
-		LOG_SEC=$(ssh ${node} "head -1 ${LOGS_DIR}/debug.log | cut -d ' ' -f2 | cut -d ':' -f3 | cut -d '.' -f1")
+		LOG_DATE=$(ssh ${n} "head -1 ${LOGS_DIR}/debug.log | cut -d ' ' -f1")
+		LOG_HOUR=$(ssh ${n} "head -1 ${LOGS_DIR}/debug.log | cut -d ' ' -f2 | cut -d ':' -f1")
+		LOG_MIN=$(ssh ${n} "head -1 ${LOGS_DIR}/debug.log | cut -d ' ' -f2 | cut -d ':' -f2")
+		LOG_SEC=$(ssh ${n} "head -1 ${LOGS_DIR}/debug.log | cut -d ' ' -f2 | cut -d ':' -f3 | cut -d '.' -f1")
 
 
-		ssh ${node} "mv ${LOGS_DIR}/debug.log ${NEW_LOGS_DIR}/debug_${LOG_DATE}_${LOG_HOUR}_${LOG_MIN}_${LOG_SEC}.log"
+		ssh ${n} "mv ${LOGS_DIR}/debug.log ${NEW_LOGS_DIR}/debug_${LOG_DATE}_${LOG_HOUR}_${LOG_MIN}_${LOG_SEC}.log"
 
 		#########################
 		#	Rename database
 		#########################
-		if ssh ${node} "[ -d ${DB_DIR}/db ]"
+		if ssh ${n} "[ -d ${DB_DIR}/db ]"
 		then
-			ssh ${node} "mkdir ${NEW_DB_DIR}/db_${LOG_DATE}_${LOG_HOUR}_${LOG_MIN}_${LOG_SEC}"
-			ssh ${node} "mv ${DB_DIR}/db/* ${NEW_DB_DIR}/db_${LOG_DATE}_${LOG_HOUR}_${LOG_MIN}_${LOG_SEC}/"
+			ssh ${n} "mkdir ${NEW_DB_DIR}/db_${LOG_DATE}_${LOG_HOUR}_${LOG_MIN}_${LOG_SEC}"
+			ssh ${n} "mv ${DB_DIR}/db/* ${NEW_DB_DIR}/db_${LOG_DATE}_${LOG_HOUR}_${LOG_MIN}_${LOG_SEC}/"
 		else 
 			echo "No database"
 		fi
@@ -97,9 +97,9 @@ do
 		########################
 		# Rename stdout logs
 		########################
-		if ssh ${node} "[ -f ${STDOUTLOGS_DIR}/log.out ]"
+		if ssh ${n} "[ -f ${STDOUTLOGS_DIR}/log.out ]"
 		then
-			ssh ${node} "mv ${STDOUTLOGS_DIR}/log.out ${NEW_STDOUTLOGS_DIR}/log_${LOG_DATE}_${LOG_HOUR}_${LOG_MIN}_${LOG_SEC}.out"
+			ssh ${n} "mv ${STDOUTLOGS_DIR}/log.out ${NEW_STDOUTLOGS_DIR}/log_${LOG_DATE}_${LOG_HOUR}_${LOG_MIN}_${LOG_SEC}.out"
 		else 
 			echo "No stdout log"
 		fi
@@ -107,9 +107,9 @@ do
 		########################
 		# Rename GRPC logs
 		########################
-		if ssh ${node} "[ -f ${STDOUTLOGS_DIR}/log.out ]"
+		if ssh ${n} "[ -f ${STDOUTLOGS_DIR}/log.out ]"
 		then
-			ssh ${node} "mv ${GRPCLOGS_DIR}/log.out ${NEW_GRPCLOGS_DIR}/log_${LOG_DATE}_${LOG_HOUR}_${LOG_MIN}_${LOG_SEC}.out"
+			ssh ${n} "mv ${GRPCLOGS_DIR}/log.out ${NEW_GRPCLOGS_DIR}/log_${LOG_DATE}_${LOG_HOUR}_${LOG_MIN}_${LOG_SEC}.out"
 		else 
 			echo "No gRPC logs"
 		fi
